1842b259b3e28df2c2a95211e20af4bd8d9f34d2
Nardberjean <Nardberjean@users.noreply.github.com>
2017-07-13 12:03:01 -0400

    Update PagedQueryTrait.php

--- PagedQueryTrait.php
+++ PagedQueryTrait.php
@@ -3,6 +3,7 @@
 namespace DbalUtil\Connection\Pagerfanta;
 
 use DbalUtil\Connection\ConnectionAbstractTrait;
+use Doctrine\DBAL\Query\QueryBuilder;
 use Pagerfanta\Adapter\DoctrineDbalAdapter;
 use Pagerfanta\Pagerfanta;
 use PagerfantaAdapters\Doctrine\DBAL\TwoModifiers;
@@ -21,13 +22,13 @@ trait PagedQueryTrait
         // $unorderedQueryBuilder = $this->getManyToManyWhereQueryBuilder($base_table, $base_id, $link_base_id, $link_table, $link_distant_id, $distant_id, $distant_table, $where);
         // TODO: do not make it twice...
 
-        $finishQueryBuilderModifier = function ($queryBuilder) use ($orderby) {
+        $finishQueryBuilderModifier = function (QueryBuilder $queryBuilder) use ($orderby) {
             if ($orderby != ''):
                 $queryBuilder->orderBy($orderby, 'ASC');
             endif;
         };
         
-        $countQueryBuilderModifier = function ($queryBuilder) {
+        $countQueryBuilderModifier = function (QueryBuilder $queryBuilder) {
             $queryBuilder->select('COUNT(DISTINCT base.uuid) AS total_results') // ->orderBy(null) does not remove orderby
                 // ->groupBy('base.term') // suggested by Postgres error // TODO: Would it be only needed for counting?            
                 ->setMaxResults(1);
@@ -42,7 +43,7 @@ trait PagedQueryTrait
     {
         $queryBuilder = $this->getWhereManyToManyToManyQueryBuilder($base_table, $base_id, $base_link_base_id, $base_link_table, $base_link_distant_id, $distant_link_base_id, $distant_link_table, $distant_link_distant_id, $distant_id, $distant_table, $where);
 
-        $countQueryBuilderModifier = function ($queryBuilder) {
+        $countQueryBuilderModifier = function (QueryBuilder $queryBuilder) {
             $queryBuilder->select('COUNT(DISTINCT distant.uuid) AS total_results')
                   ->setMaxResults(1);
         };
@@ -59,7 +60,7 @@ trait PagedQueryTrait
             $link_base_id, $link_table, $link_distant_id, $distant_id, 
             $distant_table, $where);
 
-        $countQueryBuilderModifier = function ($queryBuilder) {
+        $countQueryBuilderModifier = function (QueryBuilder $queryBuilder) {
             $queryBuilder->select('COUNT(DISTINCT base.uuid) AS total_results')
                   ->setMaxResults(1);
         };
@@ -76,7 +77,7 @@ trait PagedQueryTrait
             $link_base_id, $link_table, $link_distant_id, $distant_id, 
             $distant_table, $where);
 
-        $finishQueryBuilderModifier = function ($queryBuilder) {
+        $finishQueryBuilderModifier = function (QueryBuilder $queryBuilder) {
             $queryBuilder
                 ->groupBy('more.uuid') // suggested by Postgres error // TODO: Would it be only needed for counting?
                 ->addGroupBy('base.uuid') // suggested by Postgres error // TODO: Would it be only needed for counting?
@@ -84,7 +85,7 @@ trait PagedQueryTrait
             ;
         };
         
-        $countQueryBuilderModifier = function ($queryBuilder) { // TODO: a simplified query may improve performance!
+        $countQueryBuilderModifier = function (QueryBuilder $queryBuilder) { // TODO: a simplified query may improve performance!
             // $queryBuilder->select('COUNT(DISTINCT base.uuid) AS total_results, count(base.uuid=taxo.owned_url_uuid) AS taxocount')
             $queryBuilder->select('COUNT(DISTINCT base.uuid) AS total_results')
                   ->setMaxResults(1);
