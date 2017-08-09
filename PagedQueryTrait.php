057bd54e5eaa9dd1c51d636e54047c8592aa6aeb
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-09 09:11:46 -0400

    Update PagedQueryTrait.php

--- PagedQueryTrait.php
+++ PagedQueryTrait.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Doctrine DBAL Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace DbalUtil\Connection\Pagerfanta;
 
 use DbalUtil\Connection\ConnectionAbstractTrait;
